2eb37499b95f503dca63b3f82e062bcedc37b700
Mykhailo Popovych <popovychmykhailo00@gmail.com>
2023-06-29 17:23:34 +0300

    created file to define start DB + table

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sql_todo`.`todos` (
+  `todo` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(1024) NULL,
+  `creation_date` DATETIME NOT NULL,
+  `status` BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (`todo`));
